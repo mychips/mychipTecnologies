75c94ac3882ed447630eca075b065a307a241eb2
mychips <mychip13@hotmail.com>
2025-10-01 17:54:50 -0600

    Cambios pequeños

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
   selector: 'app-home',
   imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
