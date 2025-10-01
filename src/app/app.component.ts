97411cd8ee650173c20c574b9349a6c7e26e51d5
mychips <mychip13@hotmail.com>
2025-10-01 03:23:31 -0600

    Landing page al 80 porciento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, RouterModule, ToolbarComponent]
 })
 export class AppComponent {
   title = 'mychipTecnologia';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', component: HomeComponent, title: 'Home'
+    },
+    {
+        path: 'toolbar', component: ToolbarComponent, title: 'Toolbar'    },
+    {
+        path: '**', redirectTo: 'home', pathMatch: 'full'
+    }
+];
